af42fb1e7f509cc74131f45264ec04a923ce1b01
Vu Luong <vuluongj20@gmail.com>
2021-03-25 13:59:50 -0700

    Updated intro text for Group Unawareness demo

--- src/components/group-unawareness/demo/Demo.js
+++ src/components/group-unawareness/demo/Demo.js
@@ -7,7 +7,7 @@ import whatInput from 'what-input'
 import windowResize from 'window-resize'
 
 const content = {
-  intro: (direction) => `You\'re a machine learning engineer at a commercial bank. One day, your manager gives you a large dataset and asks you to build an algorithm that classifies potential clients into two income types: those who make more than 50K/year, and those who make less. You have access to the attributes ${direction}. Which of those attributes would you use?`,
+  intro: (direction) => `Imagine: you\'re a machine learning engineer at a commercial bank. One day, your manager gives you a large dataset and asks you to build an algorithm that classifies potential clients into two groups: those who earn more than $50K/year, and those who earn less. You have access to the attributes ${direction}. Which of those attributes would you use?`,
   introLabel: cta => `${cta.charAt(0).toUpperCase() + cta.slice(1)} on attributes to select/deselect them. When you're done, ${cta} \'Run model\' to start.`,
   trainButton: 'Run model',
   logs: {
