8ec03d946857354d425c21cf02b3e3bed9b311b1
vuluongj20 <vuluongj20@gmail.com>
2023-01-02 15:12:55 -0800

    Add link to portfolio

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -6,7 +6,12 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <p className="footer-text">(Un)Fair Machine</p>
-        <p className="footer-text">Made by Vu Luong</p>
+        <p className="footer-text">
+          Made by&nbsp;
+          <a href="https://www.vuluong.dev" target="_blank" rel="noreferrer">
+            Vu Luong
+          </a>
+        </p>
       </footer>
     )
   }
